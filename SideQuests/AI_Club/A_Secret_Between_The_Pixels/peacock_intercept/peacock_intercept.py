0431ff63bc57a4202c781052a2a7b1940c49464a
Nixalix_Code <nixmail179@gmail.com>
2026-08-01 11:26:42 +0800

    Addition

---
+++ SideQuests/AI_Club/A_Secret_Between_The_Pixels/peacock_intercept/peacock_intercept.py
@@ -0,0 +1,4 @@
+import cv2
+
+img = cv2.imread("AI_Club/A_Secret_Between_The_Pixels/peacock_intercept.png", cv2.IMREAD_COLOR)
+target_hash = "f58d36b2179b4ea7cd185bc574ccae4e9a315b445568637debe5b0c7180ab014"
---
+++ SideQuests/AI_Club/Reverse Prompt Engineering.md
@@ -0,0 +1,3 @@
+#PROMPT:
+
+Cyberpunk style streets with 3 people walking in rain with umbrellas
